f4b482c30514ac53217fa439eb985f61aee31919
Rod <rodjohnguillen@gmail.com>
2026-04-22 21:10:00 +0800

    add final logic to write output to even.txt and odd.txt

--- read_txt.py/integers_sorter.py
+++ read_txt.py/integers_sorter.py
@@ -1,9 +1,11 @@
-# Filtering logic
+# Program to separate integers into even.txt and odd.txt
 def main():
+    # Read the text file named numbers.txt
     infile = open('numbers.txt', 'r')
     content = infile.read().split()
     infile.close()
 
+    # Create lists for sorting (matches your current logic)
     evens = []
     odds = []
 
@@ -14,7 +16,19 @@ def main():
         else:
             odds.append(num)
 
-    print("Evens sorted:", evens)
-    print("Odds sorted:", odds)
+    # Create the first text file named even.txt
+    even_file = open('even.txt', 'w')
+    for number in evens:
+        even_file.write(str(number) + "\n")
+    even_file.close()
 
-main()
+    # Create the second text file named odd.txt
+    odd_file = open('odd.txt', 'w')
+    for number in odds:
+        odd_file.write(str(number) + "\n")
+    odd_file.close()
+
+    print("Success! even.txt and odd.txt have been created.")
+
+if __name__ == "__main__":
+    main()
